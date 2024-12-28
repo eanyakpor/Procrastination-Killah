10471451b8537274228c50d8608e3b391bef69f3
Emi Anyakpor <eanyakpor@gmail.com>
2024-12-27 21:52:48 -0800

    updated the readme file

--- backend/firebase.js
+++ backend/firebase.js
@@ -1,13 +1,9 @@
 const admin = require('firebase-admin');
-require('dotenv').config();
+const serviceAccount = require('./procrastination-killah-firebase-adminsdk-d0uav-56e96c5979.json'); // Adjust the path as needed
 
 admin.initializeApp({
-  credential: admin.credential.cert({
-    projectId: process.env.FIREBASE_PROJECT_ID,
-    privateKey: process.env.FIREBASE_PRIVATE_KEY.replace(/\\n/g, '\n'),
-    clientEmail: process.env.FIREBASE_CLIENT_EMAIL,
-  }),
-  storageBucket: process.env.FIREBASE_STORAGE_BUCKET,
+  credential: admin.credential.cert(serviceAccount),
+  storageBucket: serviceAccount.storageBucket,
 });
 
 const db = admin.firestore();
