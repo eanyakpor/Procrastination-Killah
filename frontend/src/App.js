8ed19fc8615637379136071f09a530338e5c85e8
Emi Anyakpor <eanyakpor@gmail.com>
2024-12-27 18:13:05 -0800

    added basic frontend feature..really basic

---
+++ frontend/src/App.js
@@ -0,0 +1,19 @@
+import './App.css';
+import React, { useState } from 'react';
+
+
+
+
+function App() {
+  return (
+    <div className="App">
+      <form>
+        Syllabis-Ai <br></br>
+        <input type="text" placeholder="Enter text here" />
+        <button type="submit">Submit</button>
+      </form>
+    </div>
+  );
+}
+
+export default App;
